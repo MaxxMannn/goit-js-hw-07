46f855cde19edadabfb475e93f6160734ab720db
MaxxMannn <116672250+MaxxMannn@users.noreply.github.com>
2023-04-03 11:48:08 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery')
+galleryContainer.insertAdjacentHTML('beforeend',createGallery(galleryItems) )
+galleryContainer.addEventListener('click',stopDefAction)
 
-console.log(galleryItems);
+function createGallery(evt) {
+    return evt.map(({preview, original, description}) => {
+        return`
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" /> </a> 
+        `
+    }).join('')
+}
+
+function stopDefAction(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return console.log('missClick')
+    };
+    var lightbox = new SimpleLightbox('.gallery a', 
+    {captions: true, 
+     captionDelay: 250 , 
+     captionsData : 'alt', 
+    });
+}
